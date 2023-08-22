9410ec3877de131219a99e385dac0bfe876f2243
Mateo Machado <mateo.machado@masglobalconsulting.com>
2023-08-22 12:54:09 -0500

    corrección examenConocimiento

---
+++ src/main/java/org/example/Animal.java
@@ -0,0 +1,54 @@
+package org.example;
+
+public class Animal {
+
+    private String nombre;
+    private String genero;
+    private double peso;
+    Animal pareja;
+
+    public Animal() {
+    }
+
+    public Animal(String nombre, double peso) {
+        this.nombre = nombre;
+        this.peso = peso;
+        this.genero = genero;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "nombre='" + nombre + '\'' +
+                ", genero='" + genero + '\'' +
+                ", peso=" + peso +
+                '}';
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public void encontrarPareja(Animal pareja) {this.pareja = pareja;
+    }
+}
